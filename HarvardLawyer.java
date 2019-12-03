6c531d6739ab9a4e332a30de5a730afe49854166
Nightscrawl <52441295+Nightscrawl@users.noreply.github.com>
2019-12-02 16:10:45 -0800

    Update HarvardLawyer.java

--- HarvardLawyer.java
+++ HarvardLawyer.java
@@ -1,10 +1,19 @@
+/* HarvardLawyer.java
+ * Remote Work - Extra Credit
+ * Extends the lawyer class for a Harvard lawyer -- fancy!
+ * Kira Abell
+ * 12/2/2019
+ * ITC 115
+ */
+
+
 import java.util.Collections;
 
 public class HarvardLawyer extends Lawyer {
     
-    public double getSalary() {
-        return super.getSalary() * 1.2;
-    }
+	public double getSalary() {
+        	return super.getSalary() * 1.2;
+    	}
 	
 	public int getVacDays() {
 		return super.getVacDays() + 3;
@@ -12,6 +21,7 @@ public class HarvardLawyer extends Lawyer {
 	
 	public String getVacForm() {
 		return String.join("", Collections.nCopies(4, super.getVacForm() ) );
-    }
+    	}
+		// generates 4 copies of the given string
     
 }
